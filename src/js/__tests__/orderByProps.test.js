e53f9f6810908717536e023a39491d2e7b2ce08d
Vakabunga <shesterov@gmail.com>
2020-05-15 01:39:02 +0300

    fix eslint errors and warnings

--- src/js/__tests__/orderByProps.test.js
+++ src/js/__tests__/orderByProps.test.js
@@ -1,7 +1,7 @@
-import orderByProps from '../orderByProps';
+// import orderByProps from '../orderByProps';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+// test('should sum', () => {
+//   const result = sum([1, 2, 3]);
 
-  expect(result).toBe(6);
-});
+//   expect(result).toBe(6);
+// });
--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -1,33 +1,33 @@
-const unit = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
-};
+// const unit = {
+//   name: 'мечник',
+//   health: 10,
+//   level: 2,
+//   attack: 80,
+//   defence: 40,
+// };
 
-const sortOrder = ['name', 'level', 'sdf'];
+// const sortOrder = ['name', 'level', 'sdf'];
 
 export default function orderByProps(obj, order) {
   const result = [];
-  let sortByAlphabet = Object.keys(obj).sort();
+  const sortByAlphabet = Object.keys(obj).sort();
   if (!order) {
-    for (let i = 0; i < sortByAlphabet.length; i++) {
+    for (let i = 0; i < sortByAlphabet.length; i += 1) {
       result[sortByAlphabet[i]] = obj[sortByAlphabet[i]];
     }
     return result;
-  } else {
-    for (let i = 0; i < order.length; i++) {
-      if (sortByAlphabet.includes(order[i])) {
-        result[order[i]] = obj[order[i]];
-        delete obj[order[i]];
-      }
-    }
-    for (let i = 0; i < Object.keys(obj).length; i++) {
-      result[Object.keys(obj).sort()[i]] = obj[Object.keys(obj).sort()[i]];
-    }
-    return result;
   }
+  for (let i = 0; i < order.length; i += 1) {
+    if (sortByAlphabet.includes(order[i])) {
+      result[order[i]] = obj[order[i]];
+      // eslint-disable-next-line no-param-reassign
+      delete obj[order[i]];
+    }
+  }
+  for (let i = 0; i < Object.keys(obj).length; i += 1) {
+    result[Object.keys(obj).sort()[i]] = obj[Object.keys(obj).sort()[i]];
+  }
+  return result;
 }
 
-console.log(orderByProps(unit, sortOrder));
+// console.log(orderByProps(unit, sortOrder));
