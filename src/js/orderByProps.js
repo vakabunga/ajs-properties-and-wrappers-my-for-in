f0e8b54164b01312a6257b06cc568e300f1108e7
Vakabunga <shesterov@gmail.com>
2020-05-22 23:30:22 +0300

    fix function. now it doesn't change income object

--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -4,24 +4,24 @@ export default function orderByProps(obj, order) {
   // если фильтрующий массив отсуутствует, просто сортируем свойства по алфавиту
   if (!order) {
     for (let i = 0; i < sortByAlphabet.length; i += 1) {
-      result[sortByAlphabet[i]] = obj[sortByAlphabet[i]];
+      result.push({ [sortByAlphabet[i]]: obj[sortByAlphabet[i]] });
     }
     return result;
   }
   /* если фильтр сортировки задан, то свойства из него записываем в результирующий массив
   (если свойства фильтра существуют в исходном массиве)
-   после - удаляем входящего объекта свойства, которые есть в фильтре */
+   после - удаляем у отсортированного по алфавиту массива ключей объекта свойства,
+   которые есть в фильтре */
   for (let i = 0; i < order.length; i += 1) {
     if (sortByAlphabet.includes(order[i])) {
-      result[order[i]] = obj[order[i]];
+      result.push({ [order[i]]: obj[order[i]] });
       // eslint-disable-next-line no-param-reassign
-      delete obj[order[i]];
+      sortByAlphabet.splice(sortByAlphabet.indexOf(order[i]), 1);
     }
   }
-  // оставшиейся свойства во входящем массиве сортируем по алфавиту
-  // и добавляем в результирующий массив
-  for (let i = 0; i < Object.keys(obj).length; i += 1) {
-    result[Object.keys(obj).sort()[i]] = obj[Object.keys(obj).sort()[i]];
+  // оставшиеся свойства в отсортированном массиве добавляем в результирующий массив
+  for (let i = 0; i < Object.keys(sortByAlphabet).length; i += 1) {
+    result.push({ [sortByAlphabet[i]]: obj[Object.keys(obj).sort()[i]] });
   }
   return result;
 }
